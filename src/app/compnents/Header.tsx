35a9d0c25270d8e5b200d67099643882fba28c58
Philip Housden <philip.houden.04@gmail.com>
2024-11-06 21:06:29 +0700

    change header background colour

--- src/app/compnents/Header.tsx
+++ src/app/compnents/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header className="bg-blue-600 p-4 text-white">
+        <header className="bg-black p-4 text-white">
             <nav className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold">BikerSky</h1>
                 <ul className="flex space-x-4">
