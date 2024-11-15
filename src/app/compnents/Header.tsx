a0f01047fc074250505dfbc24faa5cabd5cb979d
Philip Housden <philip.houden.04@gmail.com>
2024-11-15 17:56:17 +0700

    update header

--- src/app/compnents/Header.tsx
+++ src/app/compnents/Header.tsx
@@ -1,15 +1,39 @@
+"use client"
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { usePathname } from 'next/navigation';  // Use usePathname from next/navigation
 
 export default function Header() {
+    const [isClient, setIsClient] = useState(false);
+
+    // Check if we're on the client side
+    useEffect(() => {
+        setIsClient(typeof window !== 'undefined');
+    }, []);
+
+    // Prevent rendering until we're sure we're on the client side
+    if (!isClient) return null;
+
+    const currentPath = usePathname();  // Use usePathname hook from next/navigation
+
     return (
         <header className="bg-black p-4 text-white">
             <nav className="container mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-bold"><span className='text-bikeOrange'>BIKER</span> SKY</h1>
+                <h1 className="text-2xl font-bold">
+                    <span className='text-bikeOrange'>BIKER</span> SKY
+                </h1>
                 <ul className="flex space-x-4">
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/dashboard">Dashboard</Link></li>
-                {/* Add other links as needed */}
+                    <li>
+                        <Link href="/" className={currentPath === '/' ? 'text-bikeOrange' : ''}>
+                            HOME
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className={currentPath === '/about' ? 'text-bikeOrange' : ''}>
+                            ABOUT
+                        </Link>
+                    </li>
+                    {/* Add other links as needed */}
                 </ul>
             </nav>
         </header>
